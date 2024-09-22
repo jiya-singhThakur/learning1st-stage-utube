a3a292e5fdc787fee1f10c158965ee31281a9e05
jiya-singhThakur <149234830+jiya-singhThakur@users.noreply.github.com>
2024-09-22 13:58:57 +0000

    container config done

---
+++ 01_basics/charvar01.js
@@ -0,0 +1,18 @@
+const accountId=143558
+let accountEmail="jiya@google.com"
+var accountPassword="jiyasingh6789"
+accountCity="jaipur"
+let accountState;
+
+// accountId=2
+accountEmail="hc@hc.com"
+accountPassword="213450098"
+accountCity="Bengaluru"
+
+console.log(accountId);
+/*
+prefer not to use var because of issue in
+ block scope and functioal scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
