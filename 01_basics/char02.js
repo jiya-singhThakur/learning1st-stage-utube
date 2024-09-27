cede0adf239410ce22d86d81a67d64241fe5a35d
jiya <jiyasingh0604@gmail.com>
2024-09-27 21:33:53 +0530

    configure done
    repo

---
+++ 01_basics/char02.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+console.log(3+3)
+console.log("hitesh")
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+let state=null
+console.log(typeof "hitesh")
+console.log(typeof age)
+console.log(typeof null)
